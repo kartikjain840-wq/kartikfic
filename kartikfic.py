2640c2d30504ee6fa60524a4d455492b86acb4bd
kartikjain840-wq <kartikjain840@gmail.com>
2025-12-11 22:18:29 +0530

    Update kartikfic.py

--- kartikfic.py
+++ kartikfic.py
@@ -1,9 +1,7 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as px
 import time
 import re
-from duckduckgo_search import DDGS
 
 # --- PAGE CONFIGURATION (Must be first) ---
 st.set_page_config(
